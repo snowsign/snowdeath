e5239723c50cfd6ef3310bef617420675a66bcac
Skye Gibbs <skye@snowsign.net>
2025-04-24 19:26:27 -0400

    Explicitly define Logger type

--- src/main/kotlin/net/snowsign/snowdeath/Snowdeath.kt
+++ src/main/kotlin/net/snowsign/snowdeath/Snowdeath.kt
@@ -1,12 +1,13 @@
 package net.snowsign.snowdeath
 
 import net.fabricmc.api.ModInitializer
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class Snowdeath : ModInitializer {
 
     companion object {
-        val LOGGER = LoggerFactory.getLogger("snowdeath")
+        val LOGGER: Logger = LoggerFactory.getLogger("snowdeath")
     }
 
     override fun onInitialize() {
