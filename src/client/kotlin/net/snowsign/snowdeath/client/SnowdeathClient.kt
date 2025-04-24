c322c7e0c36ac2bc390203a1055c2d1659ba619d
Skye Gibbs <skye@snowsign.net>
2025-04-24 13:33:43 -0400

    Initial commit

---
+++ src/client/kotlin/net/snowsign/snowdeath/client/SnowdeathClient.kt
@@ -0,0 +1,9 @@
+package net.snowsign.snowdeath.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class SnowdeathClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/net/snowsign/snowdeath/Snowdeath.kt
@@ -0,0 +1,9 @@
+package net.snowsign.snowdeath
+
+import net.fabricmc.api.ModInitializer
+
+class Snowdeath : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
